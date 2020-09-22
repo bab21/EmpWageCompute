089e843ff81a699481ec52180c0a67882d0a7a59
bab21 <2016061@iiitdmj.ac.in>
2020-09-22 23:51:53 +0530

    Attendence

---
+++ EmpWageBuild.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class EmpWageBuild{
+	
+	public static void main(String[] args){
+		
+		System.out.println("Welcome to Employee Wage Computation");
+		
+		Random rand=new Random();
+		int prs_abs=rand.nextInt(2);
+		
+		
+	}
+}
