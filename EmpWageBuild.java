dbafb951e5937e342946c5ff57718d8eb145d85c
bab21 <2016061@iiitdmj.ac.in>
2020-09-23 00:35:18 +0530

    Starting

--- EmpWageBuild.java
+++ EmpWageBuild.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
-public class EmpWageBuilder{
+public class EmpWageBuild{
+	
+	    
 	
 	public static void main(String[] args){
 		
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		Random rand=new Random();
-		int prs_abs=rand.nextInt(2);
+		
 		
 		
 	}
 }
-
-	
